bfbb766274665f4deb393fda58e3cc71de7a185c
hfog2066 <hfog2000@yahoo.com>
2020-09-16 13:30:24 -0500

    opcode functions.

---
+++ opcod_inst4.c
@@ -0,0 +1,66 @@
+#include "monty.h"
+
+/**
+ *_rotl - rotates the first element to bottom
+ *and second to the top.
+ *
+ *@doubly: linked list head.
+ *@nline: line number;
+ *Return: 0.
+ */
+
+void _rotl(stack_t **doubly, unsigned int nline)
+{
+	stack_t *aux1 = NULL;
+	stack_t *aux2 = NULL;
+	(void)nline;
+
+	if (*doubly == NULL)
+		return;
+
+	if ((*doubly)->next == NULL)
+		return;
+
+	aux1 = (*doubly)->next;
+	aux2 = *doubly;
+
+	for (; aux2->next != NULL; aux2 = aux2->next)
+		;
+
+	aux1->prev = NULL;
+	aux2->next = *doubly;
+	(*doubly)->next = NULL;
+	(*doubly)->prev = aux2;
+	*doubly = aux1;
+}
+
+/**
+ *_rotr - reverse the stack.
+ *
+ *@doubly: linked list head.
+ *@nline: line number.
+ *Return: 0.
+ */
+
+void _rotr(stack_t **doubly, unsigned int nline)
+{
+	stack_t *aux = NULL;
+	(void)nline;
+
+	if (*doubly == NULL)
+		return;
+
+	if ((*doubly)->next == NULL)
+		return;
+
+	aux = *doubly;
+
+	for (; aux->next != NULL; aux = aux->next)
+		;
+
+	aux->prev->next = NULL;
+	aux->next = *doubly;
+	aux->prev = NULL;
+	(*doubly)->prev = aux;
+	*doubly = aux;
+}
